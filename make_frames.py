b039e145372a6a95b7aac39233dea8a0b83988f7
taruny73 <tarunr@policybazaar.com>
2019-05-20 17:58:59 +0530

    new commit

---
+++ make_frames.py
@@ -0,0 +1,68 @@
+import datetime
+import pymongo
+from collections import OrderedDict
+import pandas as pd
+from dateutil.relativedelta import relativedelta
+from concurrent.futures import ThreadPoolExecutor
+import urllib2
+import os
+import cv2
+import math
+
+myclient = pymongo.MongoClient(
+    "mongodb://PBpyUser:PBPYPolicy@10.80.30.222:27017,10.80.40.97:27017,10.80.30.121:27017/PBpy?replicaSet=rs6&readPreference=secondaryPreferred&connectTimeoutms=6000")
+db = myclient.PBpy
+date = datetime.datetime.today() + relativedelta(days=-1)
+from_date = datetime.datetime.combine(date, datetime.datetime.min.time())
+# cursor = db.inspection.find({"InspectionDate":{"$gte":from_date,'$lt':date}}).limit(20)
+cursor = db.inspection.find().limit(1).sort("$natural", -1)
+path = "/home/tarun/ankit/arvind/videos/"
+counter = 0
+
+selected_cars = ["WAGON R", "SWIFT", "DZIRE", "i 20", "i 10", "i", "WAGON"]
+
+for record in cursor:
+    print (record)
+    model = record["Model"].split(" ")[0]
+
+    if ".zip" not in record["FileUrl"] and model in selected_cars:
+        response = urllib2.urlopen(record["FileUrl"])
+        dir_path = os.path.join(path, record['InspectionId'])
+        os.makedirs(dir_path)
+        vid_path = os.path.join(dir_path, record['FileUrl'].split('/')[-2])
+        with open(vid_path, 'wb') as w:
+            w.write(response.read())
+        cap = cv2.VideoCapture(vid_path)
+        frameRate = cap.get(cv2.CAP_PROP_FPS)  # frame rate
+        totalFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        videoDuration = round(totalFrames / frameRate)
+        skipTime = videoDuration - 150
+       
+        if skipTime > 0:
+            cap.set(cv2.CAP_PROP_POS_MSEC, skipTime * 1000)
+            print (counter)
+            var_name = defaultdict(lambda :0)
+	    x = []
+            while (cap.isOpened()):
+                frameId = cap.get(cv2.CAP_PROP_POS_MSEC)  # current frame number
+                print (frameId)
+                ret, frame = cap.read()
+                if (ret != True):
+                    break
+                if (math.floor(frameId) % math.floor(frameRate) == 0):
+                    counter += 1
+                    filename = str(int(counter)) + ".jpg"
+                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    var = cv2.Laplacian(gray, cv2.CV_64F).var()
+                    var_name[filename] = {"var":var, "frame":frame, "f_name":f_name}
+                    x.append(var)
+	    x.sort()
+	    sum_avg = round(sum(x[:5])/5)
+            
+	    for image in var_name:
+            	if image["var"] > sum_avg:
+			cv2.imwrite(os.path.join(dir_path,image["f_name"]),image["frame"])
+
+    cap.release()
+    counter = 0
+    print ("Done!")
---
+++ training/cars_train.py
@@ -0,0 +1,190 @@
+import os
+import sys
+import time
+import numpy as np
+import imgaug  # https://github.com/aleju/imgaug (pip3 install imgaug)
+
+# Download and install the Python COCO tools from https://github.com/waleedka/coco
+# That's a fork from the original https://github.com/pdollar/coco with a bug
+# fix for Python 3.
+# I submitted a pull request https://github.com/cocodataset/cocoapi/pull/50
+# If the PR is merged then use the original repo.
+# Note: Edit PythonAPI/Makefile and replace "python" with "python3".
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+from pycocotools import mask as maskUtils
+
+import zipfile
+import urllib.request
+import shutil
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+
+# Path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+# DEFAULT_DATASET_YEAR = "2014"
+
+############################################################
+#  Configurations
+############################################################
+
+
+class CarsConfig(Config):
+    """Configuration for training on MS COCO.
+    Derives from the base Config class and overrides values specific
+    to the COCO dataset.
+    """
+    # Give the configuration a recognizable name
+    # NAME = "coco"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 2
+
+    # Uncomment to train on 8 GPUs (default is 1)
+    # GPU_COUNT = 8
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1+10  # COCO has 80 classes
+
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN on MS COCO.')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'evaluate' on MS COCO")
+    parser.add_argument('--dataset', required=True,
+                        metavar="/path/to/coco/",
+                        help='Directory of the MS-COCO dataset')
+    parser.add_argument('--model', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--limit', required=False,
+                        default=500,
+                        metavar="<image count>",
+                        help='Images to use for evaluation (default=500)')
+    args = parser.parse_args()
+    print("Command: ", args.command)
+    print("Model: ", args.model)
+    print("Dataset: ", args.dataset)
+    print("Year: ", args.year)
+    print("Logs: ", args.logs)
+    print("Auto Download: ", args.download)
+
+    # Configurations
+    if args.command == "train":
+        config = CarsConfig()
+    else:
+        class InferenceConfig(CarsConfig):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+            DETECTION_MIN_CONFIDENCE = 0
+
+
+        config = InferenceConfig()
+    config.display()
+
+    # Create model
+    if args.command == "train":
+        model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=args.logs)
+    else:
+        model = modellib.MaskRCNN(mode="inference", config=config,
+                                  model_dir=args.logs)
+
+    # Select weights file to load
+    if args.model.lower() == "coco":
+        model_path = COCO_MODEL_PATH
+    elif args.model.lower() == "last":
+        # Find last trained weights
+        model_path = model.find_last()
+    elif args.model.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        model_path = model.get_imagenet_weights()
+    else:
+        model_path = args.model
+
+    # Load weights
+    print("Loading weights ", model_path)
+    model.load_weights(model_path, by_name=True)
+
+    # Train or evaluate
+    if args.command == "train":
+        # Training dataset. Use the training set and 35K from the
+        # validation set, as as in the Mask RCNN paper.
+        dataset_train = CocoDataset()
+        dataset_train.load_coco(args.dataset, "train", year=args.year, auto_download=args.download)
+        if args.year in '2014':
+            dataset_train.load_coco(args.dataset, "valminusminival", year=args.year, auto_download=args.download)
+        dataset_train.prepare()
+
+        # Validation dataset
+        dataset_val = CocoDataset()
+        val_type = "val" if args.year in '2017' else "minival"
+        dataset_val.load_coco(args.dataset, val_type, year=args.year, auto_download=args.download)
+        dataset_val.prepare()
+
+        # Image Augmentation
+        # Right/Left flip 50% of the time
+        augmentation = imgaug.augmenters.Fliplr(0.5)
+
+        # *** This training schedule is an example. Update to your needs ***
+
+        # Training - Stage 1
+        print("Training network heads")
+        model.train(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE,
+                    epochs=40,
+                    layers='heads',
+                    augmentation=augmentation)
+
+        # Training - Stage 2
+        # Finetune layers from ResNet stage 4 and up
+        print("Fine tune Resnet stage 4 and up")
+        model.train(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE,
+                    epochs=120,
+                    layers='4+',
+                    augmentation=augmentation)
+
+        # Training - Stage 3
+        # Fine tune all layers
+        print("Fine tune all layers")
+        model.train(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE / 10,
+                    epochs=160,
+                    layers='all',
+                    augmentation=augmentation)
+
+    elif args.command == "evaluate":
+        # Validation dataset
+        dataset_val = CocoDataset()
+        val_type = "val" if args.year in '2017' else "minival"
+        coco = dataset_val.load_coco(args.dataset, val_type, year=args.year, return_coco=True,
+                                     auto_download=args.download)
+        dataset_val.prepare()
+        print("Running COCO evaluation on {} images.".format(args.limit))
+        evaluate_coco(model, dataset_val, coco, "bbox", limit=int(args.limit))
+    else:
+        print("'{}' is not recognized. "
+              "Use 'train' or 'evaluate'".format(args.command))
